6f8d1337b1af3420e86c66738d1ceb1dd9bca3ff
b0pez <blopez4434@gmail.com>
2022-09-06 22:26:24 -0300

    modelo user

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    varified: {
+        type: Boolean,
+        default: false
+    },
+    status: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('User', UserSchema);
